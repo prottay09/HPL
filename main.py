a6751782ef5b1c2f92dfad82615b293aa3fc0460
Prottay Deb Nath <pn@m2hycon.com>
2021-05-28 10:38:37 +0200

    updates

--- main.py
+++ main.py
@@ -32,11 +32,15 @@ def main():
     total_players = 51
     for i in range(total_players):
         edit_mode = str(input('Enter edit mode : (y/n) '))
+        if edit_mode not in ['y','n']:
+            edit_mode = str(input('Wrong input. Enter edit mode : (y/n) '))
         edit_mode = True if edit_mode == 'y' else False
         if edit_mode is True:
             remaining_balance = int(input('Remaining balance : '))
+            remaining_d_cat_balance = int(input('Remaining D-Cat balance : '))
             current_batting_power = int(input('Current batting power : '))
             current_bowling_power = int(input('Current_bowling_power : '))
+            no_of_D_catagory_players = int(input('Number of D-Cat player to be bought: '))
             players_in_team = int(input('Current number of players in the team : '))
         
         player_no = int(input('Please enter the player no : '))
@@ -49,6 +53,8 @@ def main():
             sold_price = int(input('Please enter the price: '))
             if sold_price != 0:
                 flag = str(input('Is he one of the D group : (y/n) '))
+                if flag not in ['y','n']:
+                    flag = str(input('Wrong input. Is he one of the D group : (y/n) '))
                 if flag == 'y':
                     remaining_d_cat_balance = remaining_d_cat_balance - sold_price
                     no_of_D_catagory_players = no_of_D_catagory_players - 1
@@ -69,7 +75,9 @@ def main():
 
         else:
             continue
-
+        if i == total_players-1:
+            show_current_info(player_no, player_info, players_in_team, current_batting_power, current_bowling_power, remaining_balance, remaining_d_cat_balance)
+        
 
 if __name__ == '__main__':
     main()
